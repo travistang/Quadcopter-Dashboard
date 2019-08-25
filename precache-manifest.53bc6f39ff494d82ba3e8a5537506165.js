b0f12ec72c6ea2e1b54b662cf50897043d239a79
Travis <ga86yam@mytum.de>
2019-08-25 15:15:06 +0200

    Updates

---
+++ precache-manifest.53bc6f39ff494d82ba3e8a5537506165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b48813463eb78ca244accf30a058120",
+    "url": "/Quadcopter-Dashboard/index.html"
+  },
+  {
+    "revision": "c5cc1f8bd36c43ad66c4",
+    "url": "/Quadcopter-Dashboard/static/css/main.e1b3b11b.chunk.css"
+  },
+  {
+    "revision": "a5b934c42e9b5d2dd1f8",
+    "url": "/Quadcopter-Dashboard/static/js/2.73f3f63d.chunk.js"
+  },
+  {
+    "revision": "c5cc1f8bd36c43ad66c4",
+    "url": "/Quadcopter-Dashboard/static/js/main.3704454c.chunk.js"
+  },
+  {
+    "revision": "e582abec322924c83f62",
+    "url": "/Quadcopter-Dashboard/static/js/runtime~main.cec31562.js"
+  }
+]);
